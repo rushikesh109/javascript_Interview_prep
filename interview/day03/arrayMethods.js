d078bb0121aee268bf7eb4ec06a3eda85f555ab6
rushikesh109 <mangrulerushikesh2003@gmail.com>
2025-12-30 20:26:13 +0530

    feat: array method map, reduce, filter

---
+++ interview/day03/arrayMethods.js
@@ -0,0 +1,23 @@
+//map transforms,
+//filter selects, 
+// reduce accumulates
+
+
+const newArray = array.map(n => n * 2)
+
+console.log(newArray);
+// Map is higher order function used to transform the value is can written new array with same size 
+
+const newFilter = array.filter(n => n % 2 === 0)
+
+console.log(newFilter);
+// filter is used to filter out the value with a codition is given and return the filterd value
+
+
+const newReduce =  array.reduce((a ,b) => {
+    return a + b
+}, 0)
+
+
+console.log(newReduce);
+//Takes an array → reduces it into a single value (number, object, array, anything)
