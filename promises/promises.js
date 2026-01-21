baba8463bd287f8edb9df40bada2e377e18bc334
rushikesh109 <mangrulerushikesh2003@gmail.com>
2026-01-21 12:10:27 +0530

    feat: promises

--- promises/promises.js
+++ promises/promises.js
@@ -1,21 +1,17 @@
 // const promise1 = new  Promise((resolve, reject) => {
 //     setTimeout(()=>{
-//      console.log("hey rushii");
 //      resolve()
-//     },100)
-// })
-
-const { get } = require("mongoose");
-
-// promise1.then(()=>{
-//     console.log("promise1 cosumned");
+//      console.log("hey rushii");
+//     },1000)
+// }).then(()=>{
+//     console.log("promise 1 cosumned");
 // })
 
 // new Promise((resolve, reject) => {
 //     setTimeout(()=>{
 //         console.log("hey what are u doing");
 //         resolve()
-//     },100)
+//     },1000)
 // }).then(()=>{
 //     console.log("promise 2 consumed");
 // })
@@ -79,7 +75,6 @@ const { get } = require("mongoose");
 //   }, 1000);
 // });
 
-
 // const asyncawait = async() => {
 //   try {
 //      const hey =  await promiseSix;
@@ -91,30 +86,90 @@ const { get } = require("mongoose");
 //   }
 // }
 
-
-
 // asyncawait()
 
-const getAllUser = async() =>{
-    try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+// const getAllUser = async() =>{
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-        const data =  await response.json()
-        console.log(data);
-        
-    }catch(err){
-        console.log(err);
+//         const data =  await response.json()
+//         console.log(data);
+
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+// getAllUser()
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response)=>{
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) =>{
+//         console.log(error);
+//     })
+
+// const fun = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve({
+//       name: "rushi",
+//       email: "rushi@gmail.com",
+//       age: 23,
+//     });
+//   });
+// })
+//   .then((user) => {
+//     console.log(user);
+//     return user;
+//   })
+//   .then((user) => {
+//     console.log(user.age);
+//   })
+//   .catch((err) => {
+//     console.error(err.message);
+//   })
+//   .finally(() => {
+//     console.log("finally");
+//   });
+
+// const getUserDetails = async () => {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typcode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.TypeError);
+//   }
+// };
+// getUserDetails();
+
+const errorPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = false;
+
+    if (!error) {
+      resolve({
+        username: "rushi",
+        email: "hey@gmail.com",
+      });
+    } else {
+      reject(new Error ("Something went wrong"));
     }
-}
-getAllUser()
+  }, 1000);
+});
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) =>{
-        console.log(error);
-    })
+const callfunction = async () => {
+  try {
+    const data = await errorPromise;
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log("executed");
+  }
+};
+
+callfunction()
